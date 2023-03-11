ac48e3cc41781ae95d75ddf3a12b0f2b34eee305
Arnaud Gissinger <agissing@student.42.fr>
2023-03-11 11:34:06 +0100

    [BUMP] version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='rofi-notion',
-    version='1.1.0',
+    version='1.2.0',
     description='Quickly create new Notion pages for your databases with rofi as GUI',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
